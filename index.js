6016a6c1c0a8ea443722bb5f48c8b22c86e7084c
furst <andreas.furst@gmail.com>
2020-05-28 08:41:55 +0200

    t

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ try {
   console.log(`Running ${commandToRun}!`);
   core.setOutput('status', 'success');
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context, undefined, 2)
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
